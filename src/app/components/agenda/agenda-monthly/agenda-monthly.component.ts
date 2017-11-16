1d903d2d220902429cddceff17385a35a9b3cda1
Rodrigo Campos <rodrigo@webdevel.cl>
2017-11-15 23:51:39 -0300

    - agenda with starts and ends

--- src/app/components/agenda/agenda-monthly/agenda-monthly.component.ts
+++ src/app/components/agenda/agenda-monthly/agenda-monthly.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router, ActivatedRoute, Params }   from '@angular/router';
 
 
@@ -9,9 +9,12 @@ import { Place } from '../../../model/place';
 import { Booking } from '../../../model/booking';
 import { Matrix } from '../../../model/matrix';
 
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material';
+
+import { MatDatepicker } from '@angular/material';
+import { Moment } from 'moment';
 import * as moment from 'moment';
 
-import { MatSnackBar, MatSnackBarConfig } from '@angular/material';
 import { Angular2TokenService } from 'angular2-token';
 
 @Component({
@@ -30,6 +33,15 @@ export class AgendaMonthlyComponent implements OnInit {
   nDays: number;
   ptype: number;
 
+  @ViewChild(MatDatepicker) starts_picker: MatDatepicker<Moment>;
+  @ViewChild(MatDatepicker) ends_picker: MatDatepicker<Moment>;
+
+  starts: string;
+  ends: string;
+
+  minDate: string;
+  maxDate: string;
+
   constructor(
     private route: ActivatedRoute,
     private placeService: PlaceService,
@@ -39,10 +51,14 @@ export class AgendaMonthlyComponent implements OnInit {
     private _router: Router
   ) {
     moment.locale('es');
+
+    this.starts = moment().format();
+    this.ends = moment().add(30, 'days').format();
   }
 
-  ngOnInit() {
-     this._tokenService.validateToken().subscribe(
+  ngOnInit()
+  {
+    this._tokenService.validateToken().subscribe(
       res =>      console.log("Token Valid!"),
       error =>    this._handleTokenError(error)
     );
@@ -52,39 +68,47 @@ export class AgendaMonthlyComponent implements OnInit {
       .subscribe(places => this.handleGetPlacesSuccess(places));
 
     this.route.params.subscribe(params => {
-
       if(!params['ptype'])
         this.ptype = 1;
       else
         this.ptype = +params['ptype'];
     });
 
+    this.calculateDates();
+
+  }
+
+  private calculateDates()
+  {
     this.days = new Array<Date>();
 
-    this.nDays = 31;
+    this.minDate = moment(this.starts).startOf('day').format();
+    this.maxDate = moment(this.ends).endOf('day').format();
 
-    this.bookingService.getBookingsByDayAndPlace(moment().add(0, 'days').endOf("day").toDate(), this.nDays, this.ptype).then(response => this.handleGetMatrixSuccess(response));
+    this.nDays =  moment(this.ends).startOf('day').diff(moment(this.starts).startOf('day'), 'days') + 1;
+
+    this.bookingService.getBookingsByDayAndPlace(moment(this.starts).endOf("day").toDate(), this.nDays, this.ptype).then(response => this.handleGetMatrixSuccess(response));
 
     var i = 0;
     for(i = 0; i < this.nDays; i++)
     {
-
-      var day = moment().add(i, 'days').endOf("day").toDate();
+      var day = moment(this.starts).add(i, 'days').endOf("day").toDate();
       this.days.push(day);
     }
-
   }
 
-  private handleGetPlacesSuccess(places: Place[]){
+  private handleGetPlacesSuccess(places: Place[])
+  {
     this.places = places;
   }
 
-  private handleGetMatrixSuccess(matrix: Matrix[]){
-
+  private handleGetMatrixSuccess(matrix: Matrix[])
+  {
     this.matrix = Array.from(matrix);
-
   }
-   private _handleTokenError(error: any) {
+
+  private _handleTokenError(error: any)
+  {
     var config: MatSnackBarConfig = new MatSnackBarConfig();
     config.duration = 1000;
     this.snackBar.open("Su sesión ha expirado.", undefined, config);
