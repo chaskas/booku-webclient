da09867473aef70d10eb16a03f040bd96ee18c0c
Rodrigo Campos <rodrigo@webdevel.cl>
2017-11-29 15:48:24 -0300

    - added day to highlight directive

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,42 +1,72 @@
 import { Directive, ElementRef, Input, HostListener, Renderer2 } from '@angular/core';
 
+import { Moment } from 'moment';
+import * as moment from 'moment';
+
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  @Input('appHighlight') highlightTitle: string;
+  @Input('name') name: string;
+  @Input('date') date: string;
 
   wrapper = this.renderer.createElement('div');
 
-  text = this.renderer.createText('');
+  name_div = this.renderer.createElement('div');
+  date_div = this.renderer.createElement('div');
+
+  name_text = this.renderer.createText('');
+  date_text = this.renderer.createText('');
 
   constructor(private el: ElementRef, private renderer: Renderer2) {
-    this.text = this.renderer.createText(this.highlightTitle);
+
+    moment.locale('es');
+
+
+    this.name_text = this.renderer.createText(this.name);
+    this.date_text = this.renderer.createText(moment(this.date).format('dddd, DD/MM/YYYY'));
+
+    this.renderer.setAttribute(this.wrapper, 'fxLayout', 'column');
+    this.renderer.setAttribute(this.wrapper, 'fxLayoutAlign', 'space-around center');
+
+    this.renderer.setStyle(this.name_div, 'text-align', 'center');
+
   }
 
   @HostListener('mouseenter') onMouseEnter() {
 
-    this.text = this.renderer.createText(this.highlightTitle);
+    this.name_text = this.renderer.createText(this.name);
+    this.date_text = this.renderer.createText(moment(this.date).format('dddd, DD/MM/YYYY'));
 
-    this.highlight(this.highlightTitle || '');
+    this.highlight(this.name || '', this.date || '');
 
   }
 
   @HostListener('mouseleave') onMouseLeave() {
-    this.highlight(null);
+    this.renderer.removeChild(this.wrapper, this.name);
+    this.renderer.removeChild(this.wrapper, this.date);
+    this.renderer.removeChild(this.el.nativeElement, this.wrapper);
+    this.highlight(null, null);
   }
 
-  private highlight(title: string) {
+  private highlight(name: string | null, date: string | null) {
 
-    if(title)
+    if(name != null && date != null)
     {
-      this.highlightTitle = title;
-      this.renderer.appendChild(this.wrapper, this.text);
+      this.name = name;
+      this.date = date;
+      this.renderer.appendChild(this.name_div, this.name_text);
+      this.renderer.appendChild(this.date_div, this.date_text);
+      this.renderer.appendChild(this.wrapper, this.name_div);
+      this.renderer.appendChild(this.wrapper, this.date_div);
       this.renderer.appendChild(this.el.nativeElement, this.wrapper);
     } else {
+      this.renderer.removeChild(this.name_div, this.name_text);
+      this.renderer.removeChild(this.date_div, this.date_text);
+      this.renderer.removeChild(this.wrapper, this.name_div);
+      this.renderer.removeChild(this.wrapper, this.date_div);
       this.renderer.removeChild(this.el.nativeElement, this.wrapper);
-      this.renderer.removeChild(this.wrapper, this.text);
     }
 
   }
